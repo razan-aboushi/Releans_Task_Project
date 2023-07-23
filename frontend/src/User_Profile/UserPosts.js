16183e98521251478fe6b3730d991b4e7bfadaea
razan-aboushi <103279516+razan-aboushi@users.noreply.github.com>
2023-07-22 18:24:50 -0700

    Update file

--- frontend/src/User_Profile/UserPosts.js
+++ frontend/src/User_Profile/UserPosts.js
@@ -14,7 +14,7 @@ function UserPosts()
   }, []);
 
 
-// get the post from the local storage
+  // get the post from the local storage
   useEffect(() => {
     const storedPosts = localStorage.getItem('posts');
     if (storedPosts) {
@@ -22,11 +22,13 @@ function UserPosts()
     }
   }, []);
 
+  
   // cnvert the date
   function convertDate() {
-    const date =  new Date().toLocaleDateString();
+    const date = new Date().toLocaleDateString();
     return date;
   }
+
 
   return (
     <>
@@ -37,19 +39,19 @@ function UserPosts()
       ) : (
         <div className="user-posts m-10 text-left flex flex-wrap gap-4">
           {posts.map((post) => (
-            <div className="post-card mt-5 mb-5 w-90 p-4 bg-white rounded-lg shadow-lg hover:translate-y-[-5px] transition-transform duration-300 ease-in-out" key={post.id}>
+            <div className="post-card mt-5 mb-5 w-60 p-4 bg-white rounded-lg shadow-lg hover:translate-y-[-5px] transition-transform duration-300 ease-in-out" key={post.id}>
               <div className="post-header flex items-center mb-2">
-               
                 <div>
                   <span className="username font-bold text-black">{user.username}</span>
                   <span className="created-at text-xs text-gray-500 block">{convertDate(post.created_at)}</span>
                 </div>
               </div>
               <h2 className="text-black text-xl font-bold mb-2">{post.title}</h2>
-              <p className="text-black">{post.content}</p>
+              <p className="text-black">{post.brief}</p>
             </div>
           ))}
         </div>
+
       )}
     </>
   );
